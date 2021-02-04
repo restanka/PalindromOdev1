7ed78f8ba2b5f88ec600953b2f3865580e3673d0
restanka <77694741+restanka@users.noreply.github.com>
2021-02-04 23:27:54 +0300

    Add files via upload

---
+++ Palindrom Ödev/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom_Ödev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Baslik();
+            string palin = " ";
+            string sonuc = "";
+            palin = Console.ReadLine();
+            while (palin != "q")
+            {
+              
+                if (palin.Length >= 2)
+                {
+                    sonuc = "";
+                    for (int g = palin.Length - 1; g >= 0; g--) 
+                    {
+                        sonuc += palin[g];
+                    }
+                    if (sonuc == palin)
+                    {
+                        Console.WriteLine("Girdiğiniz ifade palindromdur." + sonuc);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Girdiğiniz ifade palindrom değildir.");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("2 veya 2'den fazla deger girinizz!!!");
+                }
+
+                Console.WriteLine("..Metin veya Sayı giriniz..");               
+                palin = Console.ReadLine();
+
+
+               
+                
+            }
+            
+            
+
+
+        }
+
+         static void Baslik()
+        {
+            Console.WriteLine("..Metin veya Sayı giriniz..");
+        }
+    }
+}
---
+++ Palindrom Ödev/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Palindrom Ödev")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Palindrom Ödev")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7036b2ac-8bb4-4931-8df8-6bc1a08260e6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
